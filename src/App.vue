440bcb1c6dbabf53b2156f8622e772024aa64796
Enderman8843 <Enderman8843@users.noreply.github.com>
2025-08-13 19:28:55 +0530

    Added the base ui

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Chatbox from './components/Chat.vue';
 
 </script>
 
@@ -6,6 +7,15 @@
    <header style="border-radius: 20px; border-width: 30; border-color:white ; border-style: solid; color: white; padding: 1rem;">
     <a>WEB - AI </a>
 </header>
+
+<body>
+    <div style="display: flex; flex-direction: row;"> 
+        <div class="card" style="width:25vw; margin-right: 10px; height:80vh"></div>
+        <chatbox Heading='user'></chatbox>
+    </div>
+
+
+</body>
 
 
   
---
+++ src/components/Chat.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+
+defineProps({
+  Heading: String
+})
+
+
+</script>
+
+<template>
+  <div class="card" style="width:100%">
+    <a>{{Heading}}</a>
+
+  </div>
+</template>
